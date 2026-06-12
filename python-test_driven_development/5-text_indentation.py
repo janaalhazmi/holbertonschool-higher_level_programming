37cb77c09452c73b4fd6ebd0de5eff6551b502db
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-12 22:41:05 +0300

    Refactor text_indentation to use enumeration
    Refactor text_indentation function for clarity and efficiency.

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,19 +3,17 @@
 
 
 def text_indentation(text):
-    """Print a text with 2 new lines after '.', '?' and ':'."""
+    """Print text with 2 new lines after ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    while i < len(text):
-        print(text[i], end="")
+    start = 0
 
-        if text[i] in ".?:":
-            print("\n")
-            i += 1
-            while i < len(text) and text[i] == " ":
-                i += 1
-            continue
+    for i, c in enumerate(text):
+        if c in ".?:":
+            print(text[start:i + 1].strip())
+            print()
+            start = i + 1
 
-        i += 1
+    if start < len(text):
+        print(text[start:].strip(), end="")
