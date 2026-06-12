82206037796406774261a58609b4e778ff4725d7
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-12 22:42:38 +0300

    Refactor text_indentation for better indentation handling
    Refactor text_indentation function to improve logic and clarity.

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,17 +3,21 @@
 
 
 def text_indentation(text):
-    """Print text with 2 new lines after ., ? and :"""
+    """Print text with 2 new lines after '.', '?' and ':'."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    start = 0
+    text = text.strip()
+    i = 0
 
-    for i, c in enumerate(text):
-        if c in ".?:":
-            print(text[start:i + 1].strip())
-            print()
-            start = i + 1
+    while i < len(text):
+        print(text[i], end="")
 
-    if start < len(text):
-        print(text[start:].strip(), end="")
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+
+        i += 1
