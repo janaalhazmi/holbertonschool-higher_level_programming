c9152d1986e3cd4b93a3e06c67269e6068d7b12e
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-12 18:14:16 +0000

    Fix: task 0: Integers addition, Module is documented

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Module that contains add_integer function."""
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
