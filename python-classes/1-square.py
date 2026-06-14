e7fd84c552fc81b5e9b49084e2f9a475049e7385
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-14 11:35:14 +0000

    Add : task 1 :  Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square."""
+        self.__size = size
