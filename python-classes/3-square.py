98b3ab27e0ae09d02ebfe949dc93bdc1d661fff2
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-14 12:14:09 +0000

    Add : Task 3 : Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """This module defines a Square class."""
+
+
 class Square:
     """This class defines a square."""
+
     def __init__(self, size=0):
         """Initialize a square."""
-        if size < 0:
-            """first condition."""
-           raise ValueError('size must be >= 0')
         if type(size) is not int:
-           raise TypeError('size must be an integer')
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+   
         self.__size = size
+    def area(self):
+        return self.__size  ** 2
+        
