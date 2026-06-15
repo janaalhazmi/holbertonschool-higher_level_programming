9d477c925cca56382bf4201201ac0368b0b184b8
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-15 09:06:05 +0000

    Add : task 0 : Simple rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module defines a Square class."""
 class Rectangle:
-    """This class defines a Rectangle"""
-   
 
-   pass
+     """This class defines a Rectangle"""
+
+     pass
+   
 
 
