c0b5349964d040e2c31a9d37a887aa5a67905134
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-21 14:27:26 +0000

    Add : task 6 : Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this is modul for BaseGeometry"""
+
+
+class BaseGeometry():
+    """this is method for BaseGeometry"""
+    def area(self):
+        """raises an Exception with message"""
+
+        raise Exception("area() is not implemented")
