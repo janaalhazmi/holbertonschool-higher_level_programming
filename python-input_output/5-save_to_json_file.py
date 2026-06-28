b4d43c22af8bfc2b2b6ce8fec62a3db60142fe03
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-28 13:16:13 +0000

    Add: task 5: Save Object to a file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """this is the method"""
     with open(filename, 'w', encoding="utf-8") as file:
-         json.dump(my_obj, file)
+        json.dump(my_obj, file)
