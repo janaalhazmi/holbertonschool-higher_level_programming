96290fac55433142aaeec5c993dc1859cccccdb9
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-28 13:15:25 +0000

    Add: task 5: Save Object to a file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a modul to mae json file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """this is the method"""
+    with open(filename, 'w', encoding="utf-8") as file:
+         json.dump(my_obj, file)
