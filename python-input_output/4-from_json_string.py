75bb4aa186d4526974b9cadaa94227dd32ac5504
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-28 13:09:38 +0000

    Add: task 4: From JSON string to Object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is a modul for json from obj to str"""
+import json
+
+
+def from_json_string(my_str):
+    """this is the method for json"""
+    return json.loads(my_str)
