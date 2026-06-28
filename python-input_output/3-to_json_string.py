91cd0afd50c44ac46632a452539e51bd84c47329
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-28 13:03:28 +0000

    Add: task 3: To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is JSON method from an objict to string"""
+import json
+
+
+def to_json_string(my_obj):
+    """this is the method for json"""
+    return json.dumps(my_obj)
