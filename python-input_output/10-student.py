2edf6b5cf6389de24f5777928f0ecddf03114bd7
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-28 13:48:55 +0000

    Add: task 10:  Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary description of the instance"""
+        if type(attrs) is list:
+            new_dict = {}
+
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+
+        return self.__dict__
