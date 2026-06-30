6ec1de8ef455e5acf71742414bf9531fdd8a9afe
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-30 11:42:43 +0000

    Add: task 13: Search and update

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -11,4 +11,4 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             f.write(line)
             if search_string in line:
-                 f.write(new_string)
+                f.write(new_string)
