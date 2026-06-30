51ffdfe9f330431be2640b5282615320bd4b4cad
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-06-30 11:36:10 +0000

    Add: task 13: Search and update

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""this is a function to insert a new statment after each line"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
