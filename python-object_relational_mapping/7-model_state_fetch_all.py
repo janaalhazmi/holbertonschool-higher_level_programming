6ae22298d39a49b579482f7193b0592ff52710bf
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-07-26 12:21:46 +0000

    Add : task 7: All states via SQLAlchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Creates the states table in the database, from the State model"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
