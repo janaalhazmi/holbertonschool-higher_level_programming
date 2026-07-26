67fe3327b47fe2b1ab6a4d2cf4f4c5cb18225c41
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-07-26 12:12:52 +0000

    Add : task 4: Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
+"""Module: query database with MySQLdb."""
+
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name "
-                "FROM cities INNER JOIN states "
-                "ON cities.state_id = states.id "
-                "ORDER BY cities.id ASC")
-    rows = cur.fetchall()
+
+def query_database(username: str, password: str, database: str) -> None:
+    """Query database with given parameters."""
+    db = MySQLdb.connect(
+        host="Localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name "
+        "FROM cities "
+        "INNER JOIN states "
+        "ON cities.state_id = states.id "
+        "ORDER BY cities.id ASC"
+    )
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    query_database(argv[1], argv[2], argv[3])
