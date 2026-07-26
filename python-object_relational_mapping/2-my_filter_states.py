8189fd3bdd845b3aa41d959e107df87d8e53c7f8
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-07-26 11:57:06 +0000

    Add : task 2: Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 from sys import argv
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                            passwd=argv[2], db=argv[3], charset="utf8")
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     query = """
 SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
