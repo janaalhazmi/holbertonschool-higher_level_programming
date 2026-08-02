35459bb7e7b84add10b93ecd2625a2004e884f74
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-02 11:09:45 +0300

    Refactor City class attributes and relationships

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -2,26 +2,13 @@
 """City model with relationship"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-
 from relationship_state import Base
 
-
 class City(Base):
-    """City class"""
-    __tablename__ = "cities"
+    """Class representing the states table"""
+    __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True,
-                autoincrement=True, nullable=False)
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
-
-    state_id = Column(
-        Integer,
-        ForeignKey("states.id"),
-        nullable=False
-    )
-
-    state = relationship(
-        "State",
-        back_populates="cities"
-    )
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
