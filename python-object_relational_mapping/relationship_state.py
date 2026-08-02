18face0976abf5119998b983fd457a1d8b1350e0
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-02 11:10:37 +0300

    Update relationship_state.py

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -2,21 +2,22 @@
 """State model with relationship"""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
-from model_state import Base
+Base = declarative_base()
 
 
 class State(Base):
-    """State class"""
-    __tablename__ = "states"
+    """Class representing the states table"""
+    __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True,
-                autoincrement=True, nullable=False)
+    id = Column(Integer, nullable=False, primary_key=True,
+                autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
 
     cities = relationship(
         "City",
-        back_populates="state",
-        cascade="all, delete-orphan"
-    )
+        cascade="all, delete-orphan",
+        backref=backref("state", cascade="all"),
+        single_parent=True)
