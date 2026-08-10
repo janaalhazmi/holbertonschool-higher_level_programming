460ca928b282202bfb01d56526543a1d1c5999eb
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-10 10:52:53 +0300

    Change header click event to redHeader

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,4 +1,6 @@
 const header = document.querySelector('header');
-header.addEventListener('click', function () {
+const redHeader = document.querySelector('#red_header');
+
+redHeader.addEventListener('click', function () {
   header.style.color = '#FF0000';
 });
