d7856d6959e1621005dc85d823d3860cfd381b44
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-10 08:38:15 +0000

    Add:task 4: List of elements

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,7 @@
+const addItem = document.querySelector('#add_item');
+const list = document.querySelector('.my_list');
+addItem.addEventListener('click', function () {
+    const item = document.createElement('li');
+    item.textContent = 'Item';
+    list.appendChild(item);
+});
